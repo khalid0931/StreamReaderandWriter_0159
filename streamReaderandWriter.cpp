669723bf5f1d91e0529c738139ac9f29dafe60a6
khalid0931 <mutiarif53@gmail.com>
2026-06-01 18:36:21 +0700

    menambahkan perulangan untuk memunculkan setiap baris

--- streamReaderandWriter.cpp
+++ streamReaderandWriter.cpp
@@ -41,4 +41,7 @@ int main()
     //kondisi jika file ada
     if (infile.is_open())
     {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
     
