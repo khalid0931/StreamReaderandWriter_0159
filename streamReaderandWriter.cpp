d05064e7ccde1966c527788202a4d0260feae757
khalid0931 <mutiarif53@gmail.com>
2026-06-01 18:33:31 +0700

    membuat unlimited loop untuk menulis file

--- streamReaderandWriter.cpp
+++ streamReaderandWriter.cpp
@@ -13,3 +13,9 @@ int main()
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
+    //unlimited loop untuk menulis file
+    while (true)
+    {
